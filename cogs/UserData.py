6ee82c557d065413ade3e4f87826025a5987a3e3
stackheapderivative <anbingwen@proton.me>
2026-04-04 16:09:34 -0500

    Separate cogs into folder, OOPify main.py by making start of bot be a class.

---
+++ cogs/UserData.py
@@ -0,0 +1,62 @@
+import discord
+from discord.ext import commands, tasks
+from datetime import datetime, time
+
+looptime = time(hour=0, minute=0) #lets us run the data ingestion every 24 hours.
+
+#arrays that are used to categorize roles:
+admin = ['Inquisitor','Lexmechanicus (mod team)','Enforcer Primaris','Leadership','Enforcer']
+leadership = ['Mission Department Lead','Interrogator','Enforcer Primaris','Inquisitor','Leadership','Team Leader','Tech priest']
+detachment = ['Defiance (Armor)','Vengeance (Air)','Harlequin (Actor)', 'Medicae', 'Zeal squad', 'Redemption Squad']
+rank = ['Team Leader', 'Squad Leader', 'Trooper', 'Scion', 'Schola', 'Scion', 'Acolyte', 'Agent','Reserves']
+department = ['Zeus', 'Archivist','Eden','Enginseer','Harlequin (Actor)']
+qualifications = ['BCT','Heavy Weapon: AP','Heavy Weapons: AT', 'Marksmen','Grenadier','Demo','Medical','Vox','Test-Taker']
+misc = []
+ignored = ['\\\\\\ Other Duties and Titles >>>','<<< Other Game Interests ///', '///Quals>>>', 'new role', '<<< Rank & Detachment ///','Reaction Roles']
+
+
+class UserData(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        #organization arrays
+        self.discordIDarr = []
+        self.usernamesArr = []
+        self.dateJoinedArr = []
+        self.rolesArr = []
+        #date of today
+        
+
+    #initialize bot
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.client.change_presence(activity=discord.Game("Testing bot! Online for testing!"))
+        print('IFA Bot is operational!')
+
+
+    @commands.command()
+    async def test_users(self, ctx, discordIDarr,usernamesArr,rolesArr,dateJoinedArr):
+        for u in ctx.guild.members: #gets users in the guild
+            usernamesArr.append([u.name]) #gets username
+            discordIDarr.append([u.id]) #gets discord id
+            dateJoinedArr.append([u.joined_at.strftime('%d-%m-%Y')]) #gets datetime of when user joined
+            for r in ctx.guild.roles: #gets the roles of users
+                if r in u.roles and r.name != '@everyone':
+                    rolesArr.append([r.name])
+        await ctx.send(f'TEST COMMAND:\nUSERS:{usernamesArr}\nID:{discordIDarr}\nJOINED:{dateJoinedArr}\nROLES:{rolesArr}\n')
+
+    @tasks.loop(time=looptime)
+    async def ingest_data(self, ctx, discordIDarr,usernamesArr,rolesArr,dateJoinedArr):
+        for u in ctx.guild.members: #gets users in the guild
+            usernamesArr.append([u.name]) #gets username
+            discordIDarr.append([u.id]) #gets discord id
+            dateJoinedArr.append([u.joined_at.strftime('%d-%m-%Y')]) #gets datetime of when user joined
+        for r in ctx.guild.roles: #gets the roles of users
+            if r in u.roles and r.name != '@everyone':
+                rolesArr.append([r.name])
+    
+    def organize_data(self, discordIDarr,usernamesArr,rolesArr,dateJoinedArr):
+        pass
+
+
+async def setup(client):
+    await client.add_cog(UserData(client))
--- main.py
+++ main.py
@@ -1,96 +1,29 @@
 import discord
 import os
 import random
-import time
 import database
-from datetime import datetime, timezone, time
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
-
-
-#get token
-load_dotenv()
-intents = discord.Intents.default()
-intents.members = True
-
-
-#intents section
-intents.message_content = True
-client = commands.Bot(command_prefix="?", intents=intents)
-token = os.getenv('TOKEN')
-
-
-#initialize bot
-@client.event
-async def on_ready():
-    print("Logged in as a bot {0.user}".format(client))
+from cogs.UserData import UserData #cog for handling user data
 
 '''
-get users information, such as name, id, and roles.
+Client class:
+This class is used to start the bot and load all cogs the bot requires.
+The reason for this usage is due to how OOPified discordpy was since 2.0, therefore, cannot load cogs without async.
 '''
-
-#organization arrays
-discordIDarr = []
-usernamesArr = []
-dateJoinedArr = []
-rolesArr = []
-
-#arrays that are used to categorize roles:
-admin = ['Inquisitor','Lexmechanicus (mod team)','Enforcer Primaris','Leadership','Enforcer']
-leadership = ['Mission Department Lead','Interrogator','Enforcer Primaris','Inquisitor','Leadership','Team Leader','Tech priest']
-detachment = ['Defiance (Armor)','Vengeance (Air)','Harlequin (Actor)', 'Medicae', 'Zeal squad', 'Redemption Squad']
-rank = ['Team Leader', 'Squad Leader', 'Trooper', 'Scion', 'Schola', 'Scion', 'Acolyte', 'Agent','Reserves']
-department = ['Zeus', 'Archivist','Eden','Enginseer','Harlequin (Actor)']
-qualifications = ['BCT','Heavy Weapon: AP','Heavy Weapons: AT', 'Marksmen','Grenadier','Demo','Medical','Vox','Test-Taker']
-misc = []
-ignored = ['\\\\\\ Other Duties and Titles >>>','<<< Other Game Interests ///', '///Quals>>>', 'new role', '<<< Rank & Detachment ///','Reaction Roles']
-
-#date of today
-time = datetime.time(hour=24, minute=0, tzinfo=datetime.UTC) #lets us run the data ingestion every 24 hours.
-       
-# @client.command()
-# async def test_users(ctx):
-#     for u in ctx.guild.members: #gets users in the guild
-#         usernamesArr.append([u.name]) #gets username
-#         discordIDarr.append([u.id]) #gets discord id
-#         dateJoinedArr.append([u.joined_at.strftime('%d-%m-%Y')]) #gets datetime of when user joined
-#         for r in ctx.guild.roles: #gets the roles of users
-#             if r in u.roles and r.name != '@everyone':
-#                 rolesArr.append([r.name])
-#     await ctx.send(f'TEST COMMAND:\nUSERS:{usernamesArr}\nID:{discordIDarr}\nJOINED:{dateJoinedArr}\nROLES:{rolesArr}\n')
-
-
-#Class for the ingestion of users and future interactions with users
-class UserData(commands.Cog):
+class Client(commands.Bot):
     def __init__(self):
-        pass
+        intents = discord.Intents.default() #intents
+        intents.members = True
+        super().__init__(command_prefix="?", intents=intents)
 
-    @client.command()
-    async def test_users(self, ctx):
-        for u in ctx.guild.members: #gets users in the guild
-            usernamesArr.append([u.name]) #gets username
-            discordIDarr.append([u.id]) #gets discord id
-            dateJoinedArr.append([u.joined_at.strftime('%d-%m-%Y')]) #gets datetime of when user joined
-            for r in ctx.guild.roles: #gets the roles of users
-                if r in u.roles and r.name != '@everyone':
-                    rolesArr.append([r.name])
-        await ctx.send(f'TEST COMMAND:\nUSERS:{usernamesArr}\nID:{discordIDarr}\nJOINED:{dateJoinedArr}\nROLES:{rolesArr}\n')
-
-    @tasks.loop(time=time)
-    async def ingest_data(self, ctx):
-        for u in ctx.guild.members: #gets users in the guild
-            usernamesArr.append([u.name]) #gets username
-            discordIDarr.append([u.id]) #gets discord id
-            dateJoinedArr.append([u.joined_at.strftime('%d-%m-%Y')]) #gets datetime of when user joined
-        for r in ctx.guild.roles: #gets the roles of users
-            if r in u.roles and r.name != '@everyone':
-                rolesArr.append([r.name])
-        #FIXME: Add functionalities to organize data here, maybe call a real command that isn't a client.command
-        #which can organize the data into the sql database.
-
-
-
-
+    #runs automatically before bot starts to load in cogs
+    async def setup_hook(self):
+        print('Running setup hook......')
+        await self.load_extension("cogs.UserData")
 
 if __name__ == "__main__":
+    client = Client()
+    load_dotenv() #get token
+    token = os.getenv('TOKEN')
     client.run(token)
